f639c333d92a1b433520a7e0fec68b1ad0a5bd6b
Hyunsu Kim <ksoksl0711@gmail.com>
2021-11-16 23:24:22 +0000

    Add event listener of textarea via Jquery

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  
+  $("#tweet-text").on('input', function(event) {
+
+    const count = $(this).closest("form").find(".counter")
+    
+    const letterCount = $(this).val().length;
+
+    $(count).text(140 - letterCount);
+
+    if ( letterCount > 140 ) {
+      $(count).addClass("red");
+    } else {
+      $(count).removeClass("red");
+    }
+  });
+});
