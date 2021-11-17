4d75f30cb0a24ce4f7b22b50df86852d00bd8a3c
Hyunsu Kim <ksoksl0711@gmail.com>
2021-11-17 21:04:49 +0000

    Add renderTweet function that loops data(array) then append each index of data

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,7 +1,38 @@
 $(document).ready(function () {
+  // Fake data taken from initial-tweets.json
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd"
+      },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
+
+  const renderTweets = (tweets) => {
+    tweets.forEach((user) => $('.user-tweet').append(createTweetElement(user)))
+  };
 
   const createTweetElement = (tweet) => {
-    return `<article class="tweet">
+    let $tweet =
+      `<article class="tweet">
     <header class="tweet-header">
       <div>
         <img src=${tweet.user.avatars} width="60" height="60">
@@ -13,7 +44,7 @@ $(document).ready(function () {
       <p>${tweet.content.text}</p>
     </main>
     <footer>
-      <span>${tweet.created_at} days ago</span>
+      <span class="need_to_be_rendered">${timeago.format(`${tweet.created_at}`)}</span>
       <div class="icon">
         <i class="fa fa-flag"></i>
         <i class="fa fa-retweet"></i>
@@ -21,20 +52,8 @@ $(document).ready(function () {
       </div>
     </footer>
   </article>`;
-  };
 
-  // Test / driver code (temporary)
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
+    return $tweet;
   };
-  const $tweet = createTweetElement(tweetData);
-  $('.user-tweet').append($tweet);
+  renderTweets(data);
 });
