e79773d4d250b03fbfb206a9d7b1790065238ae6
Hyunsu Kim <ksoksl0711@gmail.com>
2021-11-18 01:10:29 +0000

    Add a function of  AJAX GET request that receives data as JSON and append data to HTML

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,31 +1,4 @@
 $(document).ready(function () {
-  // Fake data taken from initial-tweets.json
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
-
   // Function appends each tweet to .user-tweet from an array of tweets
   const renderTweets = (tweets) => {
     tweets.forEach((user) => $('.user-tweet').append(createTweetElement(user)))
@@ -55,11 +28,9 @@ $(document).ready(function () {
     </footer>
   </article>`;
     return $tweet;
-  };  
+  };
 
-  renderTweets(data);
-
-  // AJAX POST request
+  // AJAX POST request, data will be serialized
   $("form").submit(event => {
     event.preventDefault();
     $.ajax({
@@ -74,4 +45,18 @@ $(document).ready(function () {
       }
     })
   })
+
+  // AJAX GET request from "/tweets" and receives array of tweets as JSON 
+  const loadtweets = () => {
+    $.ajax({
+      url: "/tweets",
+      method: "GET",
+      dataType: "json"
+    })
+      .then((data) => {
+        // append data to HTML
+        renderTweets(data);
+      })
+  }
+  loadtweets();
 });
