a81bba7b6177010f3025fb1891e57b7ebbe388c3
Hyunsu <ksoksl0711@gmail.com>
2021-12-20 02:44:31 -0800

    add port for heroku

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 "use strict";
 
 // Basic express setup:
+require("dotenv").config();
 
-const PORT          = 8080;
-const express       = require("express");
-const bodyParser    = require("body-parser");
-const app           = express();
+const PORT = process.env.PORT || 8080;
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -33,5 +34,5 @@ const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 app.use("/tweets", tweetsRoutes);
 
 app.listen(PORT, () => {
-  console.log("Example app listening on port " + PORT);
+	console.log("Example app listening on port " + PORT);
 });
